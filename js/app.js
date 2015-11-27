7964718301a74b7e1016e8bb0639a45a9fc5a1ae
Marius Banea <marius@w-d-s.co.uk>
2015-11-27 21:55:20 +0000

    Final commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function () { 
+	// This function is to get the data from youtube API and display it on the page
+	function getResults(query) {
+		$.getJSON("https://www.googleapis.com/youtube/v3/search",
+			{
+				"part": "snippet",
+				"key": "AIzaSyCclIq-RF7zhCJ_JnoXJBLdGvz-v2nzCB0",
+				"q": query 
+			},
+			function (data) {
+				if (data.pageInfo.totalResults == 0) {
+					alert("No videos found!");
+				}
+				// If there are no results it will just empty the list
+				displaySearchResults(data.items);
+			}
+
+		);
+	}
+
+	function displaySearchResults(videos) {
+		var html = "";
+		$.each(videos, function (index, video) {
+			// append li to ul
+			console.log(video.snippet.thumbnails.medium.url);
+			html = html + "<li><p>" + video.snippet.title +
+				"</p><img src='" +  video.snippet.thumbnails.high.url + "'/></li>" ;
+
+		});
+		$("#search-results ul").html(html);
+	}
+
+	$("#search-form").submit(function (event) {
+		event.preventDefault();
+		getResults($("#query").val());
+	});
+});
